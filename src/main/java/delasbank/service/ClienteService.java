980d926ed25bae5a0ce8014fbc4afd3bfa3755fa
Carmen Marchesi <cristiane.carmen.silva@gmail.com>
2022-11-08 21:12:46 -0300

    Criação de Classes no service

---
+++ src/main/java/delasbank/service/ClienteService.java
@@ -0,0 +1,30 @@
+package delasbank.service;
+
+import delasbank.model.Cliente;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ClienteService {
+
+    public void cadastrarCliente(){
+        //Conta save (Conta Conta);
+    }
+
+    public void listarClientes(){
+        //List<Cliente> findAll();
+    }
+
+    public void listarClienteId(){
+        //Conta findById (Long id) ;
+    }
+
+    public void editarCliente(){
+
+    }
+
+    public void deletarCliente(){
+
+    }
+
+}
---
+++ src/main/java/delasbank/service/TransferenciaService.java
@@ -0,0 +1,15 @@
+package delasbank.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransferenciaService {
+
+    public void listaTransferencias(){
+
+    }
+
+    public void realizarTransferencia(){
+
+    }
+}
