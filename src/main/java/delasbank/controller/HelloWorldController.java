99ffdc01af7e261841a1915a1efcf6bee4b09de7
AzenaideFernandes <azenaidefkt@gmail.com>
2022-11-11 22:30:54 -0400

    teste

--- src/main/java/delasbank/controller/HelloWorldController.java
+++ src/main/java/delasbank/controller/HelloWorldController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/")
-    public String hello(){
-
-        return "Olá, mundo!";
-    }
+//    @GetMapping("/")
+//    public String hello(){
+//
+//        return "Olá, mundo!";
+//    }
 
 }
