5dfd37b5c04d7700a3eefb8b52f3591ed0f136e0
Carmen Marchesi <cristiane.carmen.silva@gmail.com>
2022-11-15 15:40:57 -0300

    Implementação das classes da package service

--- src/main/java/delasbank/service/TransferenciaService.java
+++ src/main/java/delasbank/service/TransferenciaService.java
@@ -1,6 +1,8 @@
 package delasbank.service;
 
 import delasbank.model.Transferencia;
+import delasbank.repository.TransferenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,13 +10,14 @@ import java.util.List;
 @Service
 public class TransferenciaService {
 
-    public void listaTransferencias(){
-        //Transferencia findById(Long id);
+    @Autowired
+    private TransferenciaRepository tr;
 
-
+    public List<Transferencia> listaTransferencias(){
+        return tr.findAll();
     }
 
-    public void realizarTransferencia(){
-
+    public Transferencia realizarTransferencia(Transferencia transf){
+        return tr.save(transf);
     }
 }
